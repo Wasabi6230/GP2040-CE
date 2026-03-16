957868367bd726ea9c6d7109b41b7dd40bbdd634
Wasabi6230 <github@jwr.nz>
2026-03-16 20:21:17 +1300

    p1

--- compile_proto.cmake
+++ compile_proto.cmake
@@ -12,6 +12,7 @@ function (compile_proto)
 	add_custom_command(
 		DEPENDS ${CMAKE_SOURCE_DIR}/lib/nanopb/extra/requirements.txt
 		COMMAND ${Python3_EXECUTABLE} -m venv ${VENV}
+		COMMAND ${VENV_BIN_DIR}/python -m pip install --upgrade pip setuptools wheel
 		COMMAND ${VENV_BIN_DIR}/pip --disable-pip-version-check install -r ${CMAKE_SOURCE_DIR}/lib/nanopb/extra/requirements.txt
 		COMMAND ${VENV_BIN_DIR}/pip freeze > ${VENV_FILE}
 		OUTPUT ${VENV_FILE}
